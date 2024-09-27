d63c41fe4b973990a03d20656e26b9d3f23a4e18
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-09-26 22:54:22 -0300

    Implement basic data handler structure

--- gateway/dataHandler.go
+++ gateway/dataHandler.go
@@ -1,6 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
 func (g *gateway) startDataHandler() {
-	// aca iniciaria el hilo del data handler
-	// asignado: juli
+	address := fmt.Sprintf(":%v", g.config.dataEndpointPort)
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("failed to bind socket: %v", err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+
+		go func(conn net.Conn) {
+			// read ID
+			// send OK if valid
+			// send ERR if invalid
+			// read DATA (Game/Review)
+			// send to Rabbit
+		}(conn)
+	}
 }
