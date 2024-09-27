91b4165aae6e832e805d23489ea701d2d86ea2f0
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-09-26 22:35:47 -0300

    Add middleware basic messages

--- middleware/messages.go
+++ middleware/messages.go
@@ -1,1 +1,60 @@
 package middleware
+
+type MessageTag int
+
+const (
+	RequestHelloTag MessageTag = iota
+	RequestAcceptTag
+	DataHelloTag
+	DataAcceptTag
+	GameBatchTag
+	ReviewBatchTag
+)
+
+// Connection Handler Messages
+
+// Sent by the client to initiate a request
+type RequestHello struct {
+	// todo: data size
+}
+
+// Sent by the connection handler to accept a client's request
+type RequestAccept struct {
+	ClientID int
+}
+
+// Data Handler Messages
+
+// Sent by the client to present itself to the data handler
+type DataHello struct {
+	ClientID int
+}
+
+// Sent by the data handler to accept a client
+type DataAccept struct{}
+
+// Sent by the client to the data handler
+type GameBatch struct {
+	Games []Game
+}
+
+// Sent by the client to the data handler
+type ReviewBatch struct {
+	Reviews []Review
+}
+
+// Sent by the client to indicate that it has finished sending data
+type Finish struct{}
+
+// Generic Structures
+
+type Game struct {
+	Id   int
+	Name string
+	// todo: add fields
+}
+type Review struct {
+	Id   int
+	Text string
+	// todo: add fields
+}
