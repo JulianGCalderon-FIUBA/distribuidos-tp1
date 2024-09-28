aa4f232e755ad77ded6e79ba141688b78b133810
solmoon16 <mmoon@fi.uba.ar>
2024-09-28 20:37:44 -0300

    add serialize and deserialize for batches

--- protocol/serializer.go
+++ protocol/serializer.go
@@ -67,11 +67,44 @@ func (m *GameBatch) Tag() MessageTag {
 }
 
 func (m *GameBatch) Serialize() ([]byte, error) {
-	panic("unimplemented")
+	var msg []byte
+	var err error
+	msg, err = binary.Append(msg, binary.LittleEndian, uint32(len(m.Games)))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, array := range m.Games {
+		msg, err = binary.Append(msg, binary.LittleEndian, uint32(len(array)))
+		if err != nil {
+			return nil, err
+		}
+		msg = append(msg, array...)
+	}
+	return msg, err
 }
 
 func (m *GameBatch) Deserialize(buf []byte) error {
-	panic("unimplemented")
+	var amountArrays uint32
+	read, err := binary.Decode(buf, binary.LittleEndian, &amountArrays)
+	if err != nil {
+		return err
+	}
+	buf = buf[read:]
+
+	for i := 0; i < int(amountArrays); i++ {
+		var size uint32
+		read, err := binary.Decode(buf, binary.LittleEndian, &size)
+		if err != nil {
+			return err
+		}
+		buf = buf[read:]
+		array := buf[:size]
+		m.Games = append(m.Games, array)
+		buf = buf[size:]
+	}
+
+	return nil
 }
 
 func (m *ReviewBatch) Tag() MessageTag {
@@ -79,11 +112,44 @@ func (m *ReviewBatch) Tag() MessageTag {
 }
 
 func (m *ReviewBatch) Serialize() ([]byte, error) {
-	panic("unimplemented")
+	var msg []byte
+	var err error
+	msg, err = binary.Append(msg, binary.LittleEndian, uint32(len(m.Reviews)))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, array := range m.Reviews {
+		msg, err = binary.Append(msg, binary.LittleEndian, uint32(len(array)))
+		if err != nil {
+			return nil, err
+		}
+		msg = append(msg, array...)
+	}
+	return msg, err
 }
 
 func (m *ReviewBatch) Deserialize(buf []byte) error {
-	panic("unimplemented")
+	var amountArrays uint32
+	read, err := binary.Decode(buf, binary.LittleEndian, &amountArrays)
+	if err != nil {
+		return err
+	}
+	buf = buf[read:]
+
+	for i := 0; i < int(amountArrays); i++ {
+		var size uint32
+		read, err := binary.Decode(buf, binary.LittleEndian, &size)
+		if err != nil {
+			return err
+		}
+		buf = buf[read:]
+		array := buf[:size]
+		m.Reviews = append(m.Reviews, array)
+		buf = buf[size:]
+	}
+
+	return nil
 }
 
 func (m *Finish) Tag() MessageTag {
