5e4769d21de8fd8998cb07a15cea441f0259e875
solmoon16 <mmoon@fi.uba.ar>
2024-09-28 00:34:01 -0300

    connect to connection endpoint

---
+++ client/client.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"bufio"
+	"distribuidos/tp1/middleware"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strconv"
+)
+
+const GAMES_PATH = "./.data/games.csv"
+const REVIEWS_PATH = "./.data/reviews.csv"
+const MAX_PAYLOAD_SIZE = 8096
+
+type client struct {
+	config      config
+	id          int
+	conn        net.Conn
+	connReader  bufio.Reader
+	connWriter  bufio.Writer
+	gamesSize   int
+	reviewsSize int
+}
+
+func newClient(config config) *client {
+	client := &client{
+		config:      config,
+		gamesSize:   getFileSize(GAMES_PATH),
+		reviewsSize: getFileSize(REVIEWS_PATH),
+	}
+	return client
+}
+
+func (c *client) start() {
+	/*
+		1. conectarse al connection handler
+		2. recibir id
+		3. guardar id
+		4. conectarse al data handler
+		5. leer y enviar juegos
+		6. leer y enviar reviews
+		7. cerrar conexión con data handler
+		8. esperar en connection handler a la respuesta
+	*/
+
+	c.startConnection()
+	// to do c.startDataConnection()
+}
+
+func (c *client) startConnection() {
+	conn, err := net.Dial("tcp", c.config.connectionEndpointAddress)
+	if err != nil {
+		log.Fatalf("Could not connect to connection endpoint: %v", err)
+	}
+	c.conn = conn
+	c.connReader = *bufio.NewReader(c.conn)
+	c.connWriter = *bufio.NewWriterSize(c.conn, c.config.buffSize)
+
+	c.sendRequestHello()
+
+	msg, err := c.connReader.ReadString('\n')
+	if err != nil {
+		// handle error
+	}
+	id, err := strconv.Atoi(msg)
+	if err != nil {
+		fmt.Printf("Did not receive a valid id from endpoint")
+	}
+	c.id = id
+}
+
+func (c *client) sendRequestHello() {
+	// está bien que sea el mismo buffer? desde el otro lado saben cuándo empieza un número y cuándo termina el otro? supongo que sí porque son uint32?
+	buf := make([]byte, MAX_PAYLOAD_SIZE)
+	binary.LittleEndian.AppendUint32(buf, uint32(c.gamesSize))
+	binary.LittleEndian.AppendUint32(buf, uint32(c.reviewsSize))
+
+	size := int32(len(buf))
+	sendHeader(size, middleware.RequestHelloTag, &c.connWriter)
+	_, err := c.connWriter.Write(buf) // habria que handlear o asumimos que no hay short write?
+	if err != nil {
+		fmt.Printf("Could not write message: %v", err)
+	}
+	c.connWriter.Flush()
+}
+
+func sendHeader(size int32, tag middleware.MessageTag, writer io.Writer) {
+	err := binary.Write(writer, binary.LittleEndian, size)
+	if err != nil {
+		fmt.Printf("Failed to write size")
+	}
+	err = binary.Write(writer, binary.LittleEndian, tag)
+	if err != nil {
+		fmt.Printf("Failed to write tag")
+	}
+}
+
+func getFileSize(filePath string) int {
+	file, err := os.Stat(filePath)
+	if err != nil {
+		fmt.Printf("Could not get file info: %v", err)
+	}
+	return int(file.Size())
+}
