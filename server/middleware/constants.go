00a6bd5d0c2d77719cd3f0beb71fd4e39ab81582
solmoon16 <mmoon@fi.uba.ar>
2024-09-30 23:40:47 -0300

    add queue names

--- server/middleware/constants.go
+++ server/middleware/constants.go
@@ -2,3 +2,6 @@ package middleware
 
 const ReviewExchange string = "reviews"
 const GamesExchange string = "games"
+const GamesPartitionerQueue string = "games-partitioner"
+const GamesFilterQueue string = "games-filter"
+const ReviewsFilterQueue string = "reviews-filter"
