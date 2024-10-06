09365196ff65afa4bac9b5ce1e1fe08ea23a9738
solmoon16 <mmoon@fi.uba.ar>
2024-10-06 15:58:31 -0300

    add send any to send as interface

--- server/middleware/sender.go
+++ server/middleware/sender.go
@@ -21,3 +21,21 @@ func (m *Middleware) Send(msg any, exchange string, key string) error {
 		},
 	)
 }
+
+func (m *Middleware) SendAny(msg any, exchange string, key string) error {
+	buf, err := Serialize(&msg)
+	if err != nil {
+		return err
+	}
+
+	return m.ch.Publish(
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        buf,
+		},
+	)
+}
