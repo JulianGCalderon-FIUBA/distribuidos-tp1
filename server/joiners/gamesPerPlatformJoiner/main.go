05d259d015c1504d42e005e962e260050e18dcd4
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-10-06 20:46:54 -0300

    Update gamesPerPlatform joiner

--- server/joiners/gamesPerPlatformJoiner/main.go
+++ server/joiners/gamesPerPlatformJoiner/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"context"
+	"distribuidos/tp1/server/middleware"
+	"distribuidos/tp1/server/middleware/joiner"
 	"distribuidos/tp1/utils"
 
 	logging "github.com/op/go-logging"
@@ -10,18 +13,18 @@ import (
 var log = logging.MustGetLogger("log")
 
 type config struct {
-	RabbitIP         string
-	PartitionsNumber int
+	RabbitIP   string
+	Partitions int
 }
 
 func getConfig() (config, error) {
 	v := viper.New()
 
 	v.SetDefault("RabbitIP", "localhost")
-	v.SetDefault("PartitionsNumber", "1")
+	v.SetDefault("Partitions", "1")
 
 	_ = v.BindEnv("RabbitIP", "RABBIT_IP")
-	_ = v.BindEnv("PartitionsNumber", "PARTITIONS_NUMBER")
+	_ = v.BindEnv("Partitions", "PARTITIONS")
 
 	var c config
 	err := v.Unmarshal(&c)
@@ -29,13 +32,51 @@ func getConfig() (config, error) {
 	return c, err
 }
 
+type Platform string
+
+const (
+	Mac     Platform = "mac"
+	Linux   Platform = "linux"
+	Windows Platform = "windows"
+)
+
+type handler struct {
+	count map[Platform]int
+}
+
+func (h handler) Aggregate(c map[Platform]int) error {
+	for k, v := range c {
+		c[k] += v
+	}
+
+	return nil
+}
+
+func (h handler) Conclude() (any, error) {
+	for k, v := range h.count {
+		log.Infof("Found %v games with $v support", v, string(k))
+	}
+	return h.count, nil
+}
+
 func main() {
 	cfg, err := getConfig()
 	utils.Expect(err, "Failed to read config")
 
-	r, err := newJoiner(cfg)
+	joinCfg := joiner.Config{
+		RabbitIP:         cfg.RabbitIP,
+		Input:            middleware.GamesPerPlatformJoin,
+		Output:           "",
+		PartitionsNumber: cfg.Partitions,
+	}
+
+	h := handler{
+		count: make(map[Platform]int),
+	}
+
+	join, err := joiner.NewJoiner(joinCfg, h)
 	utils.Expect(err, "Failed to create partitioner")
 
-	err = r.run()
+	err = join.Run(context.Background())
 	utils.Expect(err, "Failed to run partitioner")
 }
