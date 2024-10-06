b154a9436978d7c81ab618b1cdb8bcb17b828ede
solmoon16 <mmoon@fi.uba.ar>
2024-10-06 17:39:53 -0300

    add interface and to csv method

--- protocol/messages.go
+++ protocol/messages.go
@@ -1,5 +1,7 @@
 package protocol
 
+import "fmt"
+
 // Sent by the client to initiate a request
 type RequestHello struct {
 	GameSize   uint64
@@ -29,6 +31,10 @@ type Batch struct {
 // Sent by the client to indicate that it has finished sending data
 type Finish struct{}
 
+type Results interface {
+	ToCSV() []string
+}
+
 // Results Messages
 type Q1Results struct {
 	Windows int
@@ -36,12 +42,37 @@ type Q1Results struct {
 	Mac     int
 }
 
+func (q1 Q1Results) ToCSV() []string {
+	q := "Query 1"
+	w := fmt.Sprintf("windows: %v", q1.Windows)
+	l := fmt.Sprintf("linux: %v", q1.Linux)
+	m := fmt.Sprintf("mac: %v", q1.Mac)
+
+	return []string{q, w, l, m}
+}
+
 type Q2Results struct {
 	TopN []string
 }
 
+func (q2 Q2Results) ToCSV() []string {
+	s := make([]string, len(q2.TopN)+1)
+	q := "Query 2"
+	s = append(s, q)
+	s = append(s, q2.TopN...)
+	return s
+}
+
 type Q3Results struct {
 	TopN []string
+}
+
+func (q3 Q3Results) ToCSV() []string {
+	s := make([]string, len(q3.TopN)+1)
+	q := "Query 3"
+	s = append(s, q)
+	s = append(s, q3.TopN...)
+	return s
 }
 
 type Q4Results struct {
@@ -49,6 +80,22 @@ type Q4Results struct {
 	EOF  bool
 }
 
+func (q4 Q4Results) ToCSV() []string {
+	s := make([]string, 2)
+	q := "Query 4"
+	s = append(s, q)
+	s = append(s, q4.Name)
+	return s
+}
+
 type Q5Results struct {
 	Percentile90 []string
+}
+
+func (q5 Q5Results) ToCSV() []string {
+	s := make([]string, len(q5.Percentile90)+1)
+	q := "Query 5"
+	s = append(s, q)
+	s = append(s, q5.Percentile90...)
+	return s
 }
