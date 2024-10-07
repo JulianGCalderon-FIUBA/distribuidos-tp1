1f5b7fa0a00298d33667ab3e7e0765ece7d38cda
Rocío <rplatini@fi.uba.ar>
2024-10-07 17:43:27 -0300

    Refactor topN aggregator

--- server/aggregators/topNHistoricAvg/main.go
+++ server/aggregators/topNHistoricAvg/main.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"container/heap"
+	"context"
+	"distribuidos/tp1/server/middleware"
+	"distribuidos/tp1/server/middleware/aggregator"
+	"distribuidos/tp1/utils"
+	"fmt"
+	"sort"
+
 	"github.com/op/go-logging"
 	"github.com/spf13/viper"
 )
@@ -8,8 +16,8 @@ import (
 var log = logging.MustGetLogger("log")
 
 type config struct {
-	RabbitIP        string
-	TopN            int
+	RabbitIP    string
+	TopN        int
 	PartitionId int
 }
 
@@ -29,16 +37,82 @@ func getConfig() (config, error) {
 	return c, err
 }
 
+type GameHeap []middleware.AvgPlaytimeGame
+
+func (g GameHeap) Len() int { return len(g) }
+func (g GameHeap) Less(i, j int) bool {
+	return g[i].AveragePlaytimeForever < g[j].AveragePlaytimeForever
+}
+func (g GameHeap) Swap(i, j int) { g[i], g[j] = g[j], g[i] }
+func (g *GameHeap) Push(x any) {
+	*g = append(*g, x.(middleware.AvgPlaytimeGame))
+}
+
+func (g *GameHeap) Pop() any {
+	old := *g
+	n := len(old)
+	x := old[n-1]
+	*g = old[0 : n-1]
+	return x
+}
+
+type handler struct {
+	topN    int
+	results GameHeap
+}
+
+func (h handler) Aggregate(g middleware.Game) error {
+	if h.results.Len() < h.topN {
+		heap.Push(&h.results, middleware.AvgPlaytimeGame{
+			Name:                   g.Name,
+			AveragePlaytimeForever: g.AveragePlaytimeForever,
+		})
+	} else if g.AveragePlaytimeForever > h.results[0].AveragePlaytimeForever {
+		heap.Pop(&h.results)
+		heap.Push(&h.results, middleware.AvgPlaytimeGame{
+			Name:                   g.Name,
+			AveragePlaytimeForever: g.AveragePlaytimeForever,
+		})
+	}
+	return nil
+}
+
+func (h handler) Conclude() ([]any, error) {
+	sortedGames := make([]middleware.AvgPlaytimeGame, 0, h.results.Len())
+	for h.results.Len() > 0 {
+		sortedGames = append(sortedGames, heap.Pop(&h.results).(middleware.AvgPlaytimeGame))
+	}
+
+	sort.Slice(sortedGames, func(i, j int) bool {
+		return sortedGames[i].AveragePlaytimeForever > sortedGames[j].AveragePlaytimeForever
+	})
+
+	for _, g := range sortedGames {
+		log.Infof("Game %v: %v", g.Name, g.AveragePlaytimeForever)
+	}
+
+	return []any{sortedGames}, nil
+}
+
 func main() {
 	cfg, err := getConfig()
-	if err != nil {
-		log.Fatalf("failed to read config: %v", err)
+	utils.Expect(err, "Failed to read config")
+
+	qName := fmt.Sprintf("%v-%v", middleware.TopNHistoricAvgQueue, cfg.PartitionId)
+	aggCfg := aggregator.Config{
+		RabbitIP: cfg.RabbitIP,
+		Input:    qName,
+		Output:   middleware.TopNHistoricAvgJQueue,
 	}
 
-	a := NewAggregator(cfg)
-
-	err = a.run()
-	if err != nil {
-		log.Fatalf("failed to get top %v historic average games: %v", cfg.TopN, err)
+	h := handler{
+		topN:    cfg.TopN,
+		results: make(GameHeap, cfg.TopN),
 	}
+
+	agg, err := aggregator.NewAggregator(aggCfg, h)
+	utils.Expect(err, "Failed to create partitioner")
+
+	err = agg.Run(context.Background())
+	utils.Expect(err, "Failed to run partitioner")
 }
