0d4bbcd43da83d713929ab75ea2b29536439111e
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2024-10-07 00:07:07 -0300

    Implement per platform (#37)
    * Implement PerPlatform aggregator
    
    * Add to docker
    
    * Ignore .data and bin/
    
    * Add perPlatformJoiner
    
    * Add per-platform joiner to docker
    
    * Add aggregator middleware
    
    * Use generic aggregator
    
    * Fix typo
    
    * Update gamesPerPlatform joiner
    
    * Update
    
    * Format
    
    * Rename nodes
    
    * Use durable false
    
    * Remove comment
    
    * Implement disordered batches support
    
    * Send results to client

---
+++ server/joiners/gamesPerPlatformJoiner/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"distribuidos/tp1/protocol"
+	"distribuidos/tp1/server/middleware"
+	"distribuidos/tp1/server/middleware/joiner"
+	"distribuidos/tp1/utils"
+	"encoding/gob"
+
+	logging "github.com/op/go-logging"
+	"github.com/spf13/viper"
+)
+
+var log = logging.MustGetLogger("log")
+
+type config struct {
+	RabbitIP   string
+	Partitions int
+}
+
+func getConfig() (config, error) {
+	v := viper.New()
+
+	v.SetDefault("RabbitIP", "localhost")
+	v.SetDefault("Partitions", "1")
+
+	_ = v.BindEnv("RabbitIP", "RABBIT_IP")
+	_ = v.BindEnv("Partitions", "PARTITIONS")
+
+	var c config
+	err := v.Unmarshal(&c)
+
+	return c, err
+}
+
+type Platform string
+
+const (
+	Mac     Platform = "mac"
+	Linux   Platform = "linux"
+	Windows Platform = "windows"
+)
+
+type handler struct {
+	count map[Platform]int
+}
+
+func (h handler) Aggregate(c map[Platform]int) error {
+	for k, v := range c {
+		h.count[k] += v
+	}
+
+	return nil
+}
+
+func (h handler) Conclude() (any, error) {
+	for k, v := range h.count {
+		log.Infof("Found %v games with %v support", v, string(k))
+	}
+	var result any = protocol.Q1Results{
+		Windows: h.count[Windows],
+		Linux:   h.count[Linux],
+		Mac:     h.count[Mac],
+	}
+	return &result, nil
+}
+
+func main() {
+	cfg, err := getConfig()
+	utils.Expect(err, "Failed to read config")
+	gob.Register(protocol.Q1Results{})
+
+	joinCfg := joiner.Config{
+		RabbitIP:         cfg.RabbitIP,
+		Input:            middleware.GamesPerPlatformJoin,
+		Output:           middleware.ResultsQueue,
+		PartitionsNumber: cfg.Partitions,
+	}
+
+	h := handler{
+		count: make(map[Platform]int),
+	}
+
+	join, err := joiner.NewJoiner(joinCfg, h)
+	utils.Expect(err, "Failed to create partitioner")
+
+	err = join.Run(context.Background())
+	utils.Expect(err, "Failed to run partitioner")
+}
