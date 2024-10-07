be7bf58b83c5569b7586ada6625cf39184a58610
solmoon16 <mmoon@fi.uba.ar>
2024-10-07 11:07:42 -0300

    join errors

--- server/gateway/connectionHandler.go
+++ server/gateway/connectionHandler.go
@@ -3,6 +3,7 @@ package main
 import (
 	"distribuidos/tp1/protocol"
 	"distribuidos/tp1/server/middleware"
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -98,10 +99,8 @@ func (g *gateway) receiveResults(conn *protocol.Conn) error {
 	for d := range deliveryCh {
 		recv, err := middleware.Deserialize[any](d.Body)
 		if err != nil {
-			err = d.Nack(false, false)
-			if err != nil {
-				return fmt.Errorf("failed to nack result: %v", err)
-			}
+			nackErr := d.Nack(false, false)
+			return errors.Join(err, nackErr)
 		}
 		switch r := recv.(type) {
 		case protocol.Q1Results:
