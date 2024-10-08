b6b6f22d477da9eeb60463a4bde6ec277fb3219e
solmoon16 <102059887+solmoon16@users.noreply.github.com>
2024-10-07 23:30:01 -0300

    More than n reviews (#46)
    * Refactor GenreExchange to follow the standard defined
    
    * add main for more than n reviews aggregator
    
    * Implement PerPlatform aggregator
    
    * Add to docker
    
    * Ignore .data and bin/
    
    * add .data and bin to dockerignore
    
    * add joiner config to docker compose
    
    * add joiner to dockerfile
    
    * add join logs to makefile
    
    * add rabbit config for joiner
    
    * add rand to test language filter faster
    
    * add more than n reviews logic
    
    * Add EOF to genre and decade filters
    
    * add results messages
    
    * add results handling for client
    
    * add defer close for connection
    
    * add results queue to rabbit config
    
    * add deserialize for results
    
    * add results handling for connection handler
    
    * add comment for debug
    
    * fix queues init
    
    * comment spam log
    
    * Add perPlatformJoiner
    
    * Add per-platform joiner to docker
    
    * init results queue for more than n reviews
    
    * add log error
    
    * handle missing batches
    
    * add acks for results and send to client
    
    * add send any to send as interface
    
    * add error msg
    
    * fix receive results
    
    * add send for interfaces rabbit
    
    * add interface and to csv method
    
    * add write results to file
    
    * remove queryname from to csv
    
    * add flush
    
    * Add aggregator middleware
    
    * fix results path
    
    * change send
    
    * rename new
    
    * Use generic aggregator
    
    * Fix typo
    
    * Update gamesPerPlatform joiner
    
    * Update
    
    * Format
    
    * Add basic grouper
    
    * fix client eof
    
    * fix eof
    
    * add games eof
    
    * Implement group by
    
    * Don't send nil results in aggregator
    
    * Init maps
    
    * End aggregator on EOF
    
    * Add q3 to compose
    
    * Add batch size support
    
    * fix merge
    
    * delete unused file
    
    * change to receive queue input names from config
    
    * add q4
    
    * add q4 using aggregator
    
    * add eof check for connection
    
    * add gob register
    
    * delete old comment
    
    * change to use map instead of array
    
    * remove debug print
    
    * add wait group done to finish go routines
    
    * join errors
    
    * fix results format
    
    * add defer wait for go routines
    
    * send msg as concrete
    
    ---------
    
    Co-authored-by: Rocío <rplatini@fi.uba.ar>
    Co-authored-by: Julián González Calderón <gonzalezcalderonjulian@gmail.com>

--- client/client.go
+++ client/client.go
@@ -21,6 +21,7 @@ type client struct {
 	reqConn  *protocol.Conn
 	dataConn *protocol.Conn
 	results  int
+	wg       sync.WaitGroup
 }
 
 func newClient(config config) *client {
@@ -35,15 +36,15 @@ func (c *client) start() error {
 	if err := c.startConnection(); err != nil {
 		return err
 	}
-	var wg sync.WaitGroup
-	wg.Add(2)
+	c.wg = sync.WaitGroup{}
+	c.wg.Add(2)
 	go func() {
 		if err := c.startDataConnection(); err != nil {
 			log.Infof("Failed to start data connection: %v", err)
 		}
 	}()
 	go c.waitResults()
-	wg.Wait()
+	c.wg.Wait()
 	return nil
 }
 
@@ -102,6 +103,7 @@ func (c *client) receiveID() error {
 
 // Starts connection with data endpoint and sends games and reviews files. When done closes connection
 func (c *client) startDataConnection() error {
+	defer c.wg.Done()
 	dataConn, err := net.Dial("tcp", c.config.DataEndpointAddress)
 	if err != nil {
 		return err
@@ -173,12 +175,17 @@ func (c *client) sendFile(filePath string) error {
 func (c *client) waitResults() {
 	log.Infof("Waiting for results")
 	defer c.reqConn.Close()
+	defer c.wg.Done()
 
 	for {
-		var results protocol.Results
+		var results any
 		err := c.reqConn.Recv(&results)
 		if err != nil {
-			log.Error("Failed to receive results message: %v", err)
+			if err == io.EOF {
+				log.Errorf("Connection closed: %v", err)
+				break
+			}
+			log.Errorf("Failed to receive results message: %v", err)
 		}
 		switch r := results.(type) {
 		case protocol.Q1Results:
@@ -195,10 +202,11 @@ func (c *client) waitResults() {
 			writeResults(r, 3)
 		case protocol.Q4Results:
 			log.Infof("Received Q4 results: %#v", results)
+			writeResults(r, 4)
 			if r.EOF {
+				log.Infof("Received Q4 EOF")
 				c.results += 1
 			}
-			writeResults(r, 4)
 		case protocol.Q5Results:
 			log.Infof("Received Q5 results: %#v", results)
 			c.results += 1
