0f909dbff57e9eb0a3b017c88fe731f28312b413
Rocío <rplatini@fi.uba.ar>
2024-10-09 11:36:38 -0300

    Refactor language filter

--- server/filters/languageFilter/main.go
+++ server/filters/languageFilter/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"context"
+	"distribuidos/tp1/server/middleware"
+	"distribuidos/tp1/server/middleware/filter"
+	"distribuidos/tp1/utils"
+
 	"github.com/op/go-logging"
+	"github.com/rylans/getlang"
 	"github.com/spf13/viper"
 )
 
 var log = logging.MustGetLogger("log")
+
+const ENGLISH = "English"
 
 type config struct {
 	RabbitIP string
@@ -23,17 +31,42 @@ func getConfig() (config, error) {
 	return c, err
 }
 
+type handler struct{}
+
+func (h handler) Filter(r middleware.Review) []filter.RoutingKey {
+	if h.isEnglish(r.Text) {
+		return []filter.RoutingKey{filter.RoutingKey(middleware.ReviewsEnglishKey)}
+	}
+
+	return []filter.RoutingKey{}
+}
+
+// Detects if received text is English or not
+func (h handler) isEnglish(text string) bool {
+	info := getlang.FromString(text)
+	return info.LanguageName() == ENGLISH
+}
+
 func main() {
 	cfg, err := getConfig()
 	if err != nil {
-		log.Fatalf("Failed to read config: %v", err)
+		log.Fatalf("failed to read config: %v", err)
 	}
-	languageFilter, err := newLanguageFilter(cfg)
-	if err != nil {
-		log.Fatalf("Failed to create new language filter: %v", err)
+
+	filterCfg := filter.Config{
+		RabbitIP: cfg.RabbitIP,
+		Input:    middleware.LanguageReviewsFilterQueue,
+		Exchange: middleware.ReviewsEnglishFilterExchange,
+		Output: map[filter.RoutingKey][]filter.QueueName{
+			filter.RoutingKey(middleware.ReviewsEnglishKey): {
+				filter.QueueName(middleware.NThousandEnglishReviewsQueue),
+			},
+		},
 	}
-	err = languageFilter.run()
-	if err != nil {
-		log.Fatalf("Failed to run language filter: %v", err)
-	}
+
+	h := handler{}
+	p, err := filter.NewFilter(filterCfg, h)
+	utils.Expect(err, "Failed to create filter")
+	err = p.Run(context.Background())
+	utils.Expect(err, "Failed to run filter")
 }
