bcedeec65d4e1c80cb181513d5b7011c04904743
solmoon16 <mmoon@fi.uba.ar>
2024-10-27 21:15:30 -0300

    use disk map

--- cmd/group-by/main.go
+++ cmd/group-by/main.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"distribuidos/tp1/middleware"
 	"distribuidos/tp1/utils"
-	"maps"
 	"os/signal"
 	"slices"
+	"strconv"
+	"sync"
 	"syscall"
 
 	logging "github.com/op/go-logging"
@@ -22,6 +23,7 @@ type config struct {
 	ReviewInput string
 	Output      string
 	BatchSize   int
+	Path        string
 }
 
 func getConfig() (config, error) {
@@ -37,6 +39,7 @@ func getConfig() (config, error) {
 	_ = v.BindEnv("ReviewInput", "REVIEW_INPUT")
 	_ = v.BindEnv("Output", "OUTPUT")
 	_ = v.BindEnv("BatchSize", "BATCH_SIZE")
+	_ = v.BindEnv("Path", "PATH")
 
 	var c config
 	err := v.Unmarshal(&c)
@@ -45,12 +48,12 @@ func getConfig() (config, error) {
 }
 
 type handler struct {
-	games           map[uint64]middleware.GameStat
-	reviews         map[uint64]int
+	diskMap         *middleware.DiskMap
 	gameSequencer   *utils.Sequencer
 	reviewSequencer *utils.Sequencer
 	batchSize       int
 	output          string
+	mu              *sync.Mutex
 }
 
 func (h *handler) handleGame(_ *middleware.Channel, data []byte) error {
@@ -62,22 +65,34 @@ func (h *handler) handleGame(_ *middleware.Channel, data []byte) error {
 	h.gameSequencer.Mark(batch.BatchID, batch.EOF)
 
 	for _, g := range batch.Data {
-		game := middleware.GameStat{
-			AppID: g.AppID,
-			Name:  g.Name,
-			Stat:  0,
-		}
-		if count, ok := h.reviews[g.AppID]; ok {
-			game.Stat = uint64(count)
-			delete(h.reviews, g.AppID)
+		h.mu.Lock()
+		saved, err := h.diskMap.Get(g.AppID)
+		if err != nil {
+			return err
 		}
 
-		h.games[g.AppID] = game
+		if saved != nil {
+			saved.Name = g.Name
+			err = h.diskMap.Insert(*saved)
+			if err != nil {
+				return err
+			}
+		} else {
+			stat := middleware.GameStat{
+				AppID: g.AppID,
+				Name:  g.Name,
+			}
+			err = h.diskMap.Insert(stat)
+			if err != nil {
+				return err
+			}
+		}
+
+		h.mu.Unlock()
 	}
 
 	if h.gameSequencer.EOF() {
 		log.Infof("Received game EOF")
-		clear(h.reviews)
 	}
 
 	return nil
@@ -92,12 +107,31 @@ func (h *handler) handleReview(ch *middleware.Channel, data []byte) error {
 	h.reviewSequencer.Mark(batch.BatchID, batch.EOF)
 
 	for _, r := range batch.Data {
-		if game, ok := h.games[r.AppID]; ok {
-			game.Stat += 1
-			h.games[r.AppID] = game
-		} else if !h.gameSequencer.EOF() {
-			h.reviews[r.AppID] += 1
+		h.mu.Lock()
+		saved, err := h.diskMap.Get(r.AppID)
+		if err != nil {
+			return err
 		}
+
+		if saved != nil {
+			saved.Stat += 1
+			err = h.diskMap.Insert(*saved)
+			if err != nil {
+				return err
+			}
+		} else {
+			stat := middleware.GameStat{
+				AppID: r.AppID,
+				Name:  "",
+				Stat:  1,
+			}
+			err = h.diskMap.Insert(stat)
+			if err != nil {
+				return err
+			}
+		}
+
+		h.mu.Unlock()
 	}
 
 	if h.reviewSequencer.EOF() {
@@ -109,11 +143,18 @@ func (h *handler) handleReview(ch *middleware.Channel, data []byte) error {
 }
 
 func (h *handler) Conclude(ch *middleware.Channel) error {
-	for k, v := range h.games {
-		if v.Stat == 0 {
-			delete(h.games, k)
-		}
+	h.mu.Lock()
+	games, err := h.diskMap.GetAll()
+	if err != nil {
+		return err
 	}
+	h.mu.Unlock()
+
+	games = slices.DeleteFunc(games, func(g middleware.GameStat) bool {
+		return g.Stat == 0
+	})
+
+	log.Infof("found %v games with more than 0 reviews", len(games))
 
 	batch := middleware.Batch[middleware.GameStat]{
 		Data:     []middleware.GameStat{},
@@ -121,8 +162,6 @@ func (h *handler) Conclude(ch *middleware.Channel) error {
 		BatchID:  0,
 		EOF:      false,
 	}
-
-	games := slices.Collect(maps.Values(h.games))
 
 	for len(games) > 0 {
 		currBatchSize := min(h.batchSize, len(games))
@@ -164,13 +203,17 @@ func main() {
 
 	nodeCfg := middleware.Config[handler]{
 		Builder: func(clientID int) handler {
+			path := middleware.Cat(cfg.Path, strconv.Itoa(clientID))
+			diskMap, err := middleware.NewDiskMap(path)
+			utils.Expect(err, "Failed to build new disk map")
+
 			return handler{
-				games:           make(map[uint64]middleware.GameStat),
-				reviews:         make(map[uint64]int),
+				diskMap:         diskMap,
 				gameSequencer:   utils.NewSequencer(),
 				reviewSequencer: utils.NewSequencer(),
 				batchSize:       cfg.BatchSize,
 				output:          output,
+				mu:              &sync.Mutex{},
 			}
 		},
 		Endpoints: map[string]middleware.HandlerFunc[handler]{
