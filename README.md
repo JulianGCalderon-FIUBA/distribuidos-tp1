f3ba348f113f96f878768c2c6806b0dbbf68375b
solmoon16 <102059887+solmoon16@users.noreply.github.com>
2024-10-27 23:31:01 -0300

    Script para generar dataset reducido (#79)
    * add script to create reduced datasets
    
    * read header before
    
    * use map instead of slice to store ids
    
    * change to use map instead of slice and fields per record = -1
    
    * add \n to print
    
    * add .results
    
    * use more games
    
    * fix eof for q4
    
    * fix eof
    
    * delete unused clientid from batch in middleware
    
    * Fix comp error
    
    * Remove unusused ignore
    
    * solve: Receive directories as input
    
    * reduce: Use another directory
    
    * Actualizar README
    
    * Agregar tildes
    
    ---------
    
    Co-authored-by: Julián González Calderón <gonzalezcalderonjulian@gmail.com>

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 # Distribuidos - TP1
 
+<!--toc:start-->
+- [Configurar cantidad de nodos por query](#configurar-cantidad-de-nodos-por-query)
+- [Ejecución con Docker](#ejecución-con-docker)
+- [Comparación de resultados](#comparación-de-resultados)
+- [Usar dataset reducido](#usar-dataset-reducido)
+<!--toc:end-->
+
 ## Configurar cantidad de nodos por query
 Para actualizar el archivo `compose.yaml` con la cantidad de nodos deseada, modificar las constantes del script `cmd/compose/main.go` y luego ejecutar el comando:
 ```bash
 make write-compose
 ```
 
-## Ejecucion con Docker
+## Ejecución con Docker
 
 Para levantar los procesos, ejecutar:
 ```bash
@@ -36,7 +43,7 @@ Luego ejecutar:
 ./cmd/detect-language/diff.sh
 ```
 
-## Comparacion de resultados
+## Comparación de resultados
 
 Para comparar los resultados, primero hay que obtener los valores de referencia. Tenemos un script de Python que los resuelve, pero necesitamos usar el mismo detector de lenguaje, para asegurar que los resultados sean los mismo. Para eso, ejecutamos:
 ```bash
@@ -45,7 +52,7 @@ go run ./scripts/filter-english-negative/main.go .data/reviews.csv .data/reviews
 
 Luego, resolvemos las consultas localmente, ejecutando:
 ```bash
-python ./scripts/solve.py
+python ./scripts/solve.py .data/ .py-results/
 ```
 Este guardara los resultados correctos en `.py-results/`
 
@@ -53,3 +60,23 @@ Para compararlos, ejecutamos:
 ```bash
 ./scripts/compare.sh .results/ .py-results/
 ```
+
+## Usar dataset reducido
+
+Primero, tenemos que generar un dataset reducido de datos. Para eso, ejecutamos:
+```bash
+go run ./scripts/reduce/main.go
+```
+
+Después, tenemos que modificar el compose para que use el dataset reducido. Tenemos que cambiar cuál carpeta de los datos se bindea al contenedor del cliente. Se puede hacer manualmente, o automáticamente ejecutando:
+```bash
+sed -i 's|./.data:/.data|./.data-reduced:/.data|' ./scripts/compose/main.go # linux
+sed -i '' 's|./.data:/.data|./.data-reduced:/.data|' ./scripts/compose/main.go # osx
+```
+
+Luego, regeneramos el compose:
+```bash
+make write-compose
+```
+
+Luego de ejecutar el sistema, podemos adaptar la sección de [comparación de resultados](#comparación-de-resultados) para utilizar el dataset reducido.
--- scripts/solve.py
+++ scripts/solve.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import os
+import argparse
 
 from pandas import DataFrame
 
+parser = argparse.ArgumentParser()
+parser.add_argument('input')
+parser.add_argument('output')
+args = parser.parse_args()
+
 # games
 
-games = pd.read_csv('.data/games.csv')
+games = pd.read_csv(f'{args.input}/games.csv')
 games_columns = ['AppID', 'Name', 'Windows', 'Mac', 'Linux', 'Genres', 'Release date', 'Average playtime forever', 'Positive', 'Negative']
 games = games.filter(games_columns, axis="columns").dropna()
 games["Genres"] = games["Genres"].str.lower()
@@ -27,17 +33,17 @@ except:
 # Q1
 
 q1_count = games[["Linux", "Mac", "Windows"]].sum().to_frame().transpose()
-q1_count.to_csv(".py-results/1.csv", header=True, index=False)
+q1_count.to_csv(f"{args.output}/1.csv", header=True, index=False)
 
 # Q2
 
 q1_games = games_indie_2010.filter(["AppID", "Name", "Average playtime forever"], axis="columns")
 q1_top = q1_games.sort_values("Average playtime forever", ascending=False).head(10)
-q1_top.to_csv(".py-results/2.csv", header=True, index=False)
+q1_top.to_csv(f"{args.output}/2.csv", header=True, index=False)
 
 # reviews
 
-reviews = pd.read_csv('.data/reviews.csv')
+reviews = pd.read_csv(f'{args.input}/reviews.csv')
 reviews_columns = ['app_id', 'review_text', 'review_score']
 reviews = reviews.filter(reviews_columns, axis="columns").dropna()
 reviews['review_text'] = reviews['review_text'].astype(str)
@@ -50,7 +56,7 @@ reviews_negative: DataFrame = reviews[reviews["review_score"] == -1] # type: ign
 print("Reviews Negative:", reviews_negative.shape[0])
 
 # We use another dataset, which was filtered in Go
-reviews_negative_ingles = pd.read_csv('.data/reviews-english-negative.csv')
+reviews_negative_ingles = pd.read_csv(f'{args.input}/reviews-english-negative.csv')
 reviews_negative_ingles = reviews_negative_ingles.filter(reviews_columns, axis="columns").dropna()
 print("Reviews Negative Ingles:", reviews_negative_ingles.shape[0])
 
@@ -68,17 +74,17 @@ def group(games, reviews) -> DataFrame:
 
 q3_grouped = group(games_indie, reviews_positive)
 q3_top = q3_grouped.sort_values("Reviews", ascending=False).head(5)
-q3_top.to_csv(".py-results/3.csv", header=True, index=True)
+q3_top.to_csv(f"{args.output}/3.csv", header=True, index=True)
 
 # Q4
 
 q4_grouped = group(games_shooter, reviews_negative_ingles)
 q4_filtered = q4_grouped[q4_grouped["Reviews"] > 5000]
-q4_filtered.sort_index().to_csv(".py-results/4.csv", header=True, index=True)
+q4_filtered.sort_index().to_csv(f"{args.output}/4.csv", header=True, index=True)
 
 # Q5
 
 q5_grouped = group(games_shooter, reviews_negative)
 percentile = q5_grouped["Reviews"].quantile(0.90)
 q5_top = q5_grouped[q5_grouped["Reviews"] >= percentile]
-q5_top.sort_index().to_csv(".py-results/5.csv", header=True, index=True)
+q5_top.sort_index().to_csv(f"{args.output}/5.csv", header=True, index=True)
