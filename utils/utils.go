4fb0ddd16916e30b9826978ab2e6d240f808f827
solmoon16 <mmoon@fi.uba.ar>
2024-11-19 20:16:41 -0300

    delete unused

--- utils/utils.go
+++ utils/utils.go
@@ -1,10 +1,6 @@
 package utils
 
 import (
-	"net"
-	"strconv"
-	"strings"
-
 	"github.com/op/go-logging"
 )
 
@@ -15,18 +11,5 @@ var log = logging.MustGetLogger("log")
 func Expect(err any, msg string) {
 	if err != nil {
 		log.Fatalf("%v: %v", msg, err)
-	}
-}
-
-func GetUDPAddr(address string) *net.UDPAddr {
-	addr := strings.Split(address, ":")
-	port, err := strconv.Atoi(addr[1])
-	if err != nil {
-		Expect(err, "Did not receive a valid address")
-	}
-
-	return &net.UDPAddr{
-		IP:   net.ParseIP(addr[0]),
-		Port: port,
 	}
 }
