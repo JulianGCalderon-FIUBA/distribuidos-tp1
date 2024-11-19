1dda855fac7d2c129a801f84a7f8439d2e5108d5
solmoon16 <mmoon@fi.uba.ar>
2024-11-18 23:56:01 -0300

    add get udp addr from string

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,12 @@
 package utils
 
-import "github.com/op/go-logging"
+import (
+	"net"
+	"strconv"
+	"strings"
+
+	"github.com/op/go-logging"
+)
 
 var log = logging.MustGetLogger("log")
 
@@ -9,5 +15,18 @@ var log = logging.MustGetLogger("log")
 func Expect(err any, msg string) {
 	if err != nil {
 		log.Fatalf("%v: %v", msg, err)
+	}
+}
+
+func GetUDPAddr(address string) *net.UDPAddr {
+	addr := strings.Split(address, ":")
+	port, err := strconv.Atoi(addr[1])
+	if err != nil {
+		Expect(err, "Did not receive a valid address")
+	}
+
+	return &net.UDPAddr{
+		IP:   net.ParseIP(addr[0]),
+		Port: port,
 	}
 }
