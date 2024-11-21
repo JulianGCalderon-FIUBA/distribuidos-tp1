27fa9120da07bedd053a56d08add4ba300826364
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2024-11-21 10:02:28 -0300

    Add abort test

--- database/snapshot_test.go
+++ database/snapshot_test.go
@@ -14,7 +14,7 @@ func expect(t *testing.T, err error) {
 	}
 }
 
-func exists(path string) (bool, error) {
+func fileExists(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err == nil {
 		return true, nil
@@ -61,9 +61,51 @@ func TestCreate(t *testing.T) {
 		t.Fatal("Snapshot should have been commited")
 	}
 
-	exists, err := exists(path.Join())
+	exists, err := fileExists(path.Join())
 	expect(t, err)
 
+	if exists {
+		t.Fatal("Snapshot should have been erased")
+	}
+}
+
+func TestCreateAbort(t *testing.T) {
+	// SETUP
+
+	database_path, err := os.MkdirTemp("", t.Name())
+	expect(t, err)
+
+	database.NewDatabase(database_path)
+
+	// EXECUTION
+
+	snapshot, err := database.NewSnapshot(database_path)
+	expect(t, err)
+
+	file, err := snapshot.Create("KEY")
+	expect(t, err)
+
+	value := []byte("VALUE")
+
+	_, err = file.Write(value)
+	expect(t, err)
+
+	err = file.Close()
+	expect(t, err)
+
+	err = snapshot.Abort()
+	expect(t, err)
+
+	// VERIFICATION
+
+	exists, err := fileExists(path.Join(database_path, database.DATA_DIR, "KEY"))
+	expect(t, err)
+	if exists {
+		t.Fatal("Snapshot should not have been commited")
+	}
+
+	exists, err = fileExists(path.Join())
+	expect(t, err)
 	if exists {
 		t.Fatal("Snapshot should have been erased")
 	}
