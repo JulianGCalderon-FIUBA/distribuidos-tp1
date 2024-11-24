784d92ca0c1f5cc2be7978874695396c7f11a27b
Rocío <rplatini@fi.uba.ar>
2024-11-24 12:52:50 -0300

    Refactor restarter to use msg ids

--- cmd/restarter/restarter.go
+++ cmd/restarter/restarter.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"context"
+	"distribuidos/tp1/restarter_protocol"
 	"distribuidos/tp1/utils"
-	"encoding/binary"
 	"encoding/csv"
 	"errors"
 	"fmt"
@@ -18,11 +18,14 @@ const CONFIG_PATH = ".node-config.csv"
 const MAX_ATTEMPTS = 3
 const MAX_PACKAGE_SIZE = 1024
 
+var ErrFallenNode = errors.New("Never got ack")
+
 type restarter struct {
-	nodes  map[string]string
-	conn   *net.UDPConn
-	mu     *sync.Mutex
-	ackMap map[string]chan bool
+	nodes     map[string]string
+	conn      *net.UDPConn
+	mu        *sync.Mutex
+	ackMap    map[uint64]chan bool
+	lastMsgId uint64
 }
 
 func readNodeConfig() (map[string]string, error) {
@@ -75,17 +78,12 @@ func newRestarter(config config) (*restarter, error) {
 		return nil, err
 	}
 
-	ackMap := make(map[string]chan bool)
-
-	for node := range nodes {
-		ackMap[node] = make(chan bool)
-	}
-
 	return &restarter{
-		nodes:  nodes,
-		conn:   conn,
-		mu:     &sync.Mutex{},
-		ackMap: ackMap,
+		nodes:     nodes,
+		conn:      conn,
+		mu:        &sync.Mutex{},
+		ackMap:    make(map[uint64]chan bool),
+		lastMsgId: 0,
 	}, nil
 }
 
@@ -104,18 +102,37 @@ func (r *restarter) start(ctx context.Context) error {
 		r.readFromSocket(ctx)
 	}()
 
-	for name := range r.nodes {
+	for node := range r.nodes {
 		wg.Add(1)
-		go func() {
+		go func(nodeName string) {
 			defer wg.Done()
-			err := r.send(name)
+			time.Sleep(5 * time.Second) // ver como hacer que espere con docker
+			err = r.monitorNode(ctx, nodeName)
 			if err != nil {
-				log.Errorf("Error monitoring node: %v", err)
+				log.Errorf("Error monitoring node %v: %v", nodeName, err)
 			}
-		}()
+		}(node)
 	}
 
 	defer wg.Wait()
+	return nil
+}
+
+func (r *restarter) monitorNode(ctx context.Context, nodeName string) error {
+	udpAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%v:%v", nodeName, r.nodes[nodeName]))
+	if err != nil {
+		return fmt.Errorf("Failed to resolve address: %v", err)
+	}
+
+	err = r.safeSend(ctx, restarter_protocol.KeepAlive{}, udpAddr)
+	if err != nil {
+		if errors.Is(err, ErrFallenNode) {
+			log.Infof("Restarting node %v", nodeName)
+			// restart node
+		} else {
+			return fmt.Errorf("Failed to send keep alive: %v", err)
+		}
+	}
 	return nil
 }
 
@@ -129,66 +146,91 @@ func (r *restarter) readFromSocket(ctx context.Context) {
 			_, _, err := r.conn.ReadFromUDP(buf)
 			if err != nil {
 				log.Errorf("Failed to read: %v", err)
-				break
+				return
+
 			}
 
-			header := utils.MsgHeader{}
-			n, err := binary.Decode(buf, binary.LittleEndian, &header)
+			packet, err := restarter_protocol.Decode(buf)
 			if err != nil {
-				log.Errorf("Failed to decode header: %v", err)
-				continue
+				log.Errorf("Failed to decode packet: %v", err)
+				return
 			}
 
-			nodeName := string(buf[n:])
-			log.Infof("Received ack of node %v", nodeName)
-
-			r.mu.Lock()
-			ch, ok := r.ackMap[nodeName]
-			defer r.mu.Unlock()
-
-			if !ok {
-				log.Errorf("Channel for node %v does not exist", nodeName)
-			} else {
-				ch <- true
+			switch packet.Msg.(type) {
+			case restarter_protocol.Ack:
+				r.handleAck(packet.Id)
 			}
 		}
 	}
 }
 
-func (r *restarter) send(nodeName string) error {
-	udpAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%v:%v", nodeName, r.nodes[nodeName]))
+func (r *restarter) safeSend(ctx context.Context, msg restarter_protocol.Message, addr *net.UDPAddr) error {
+	var err error
+	msgId := r.newMsgId()
+
+	packet := restarter_protocol.Packet{
+		Id:  msgId,
+		Msg: msg}
+
+	for attempts := 0; attempts < MAX_ATTEMPTS; attempts++ {
+		err = r.send(ctx, packet, addr)
+		if err == nil {
+			return nil
+		}
+	}
+
+	log.Errorf("Never got ack for message %d", msgId)
+	return ErrFallenNode
+}
+
+func (r *restarter) send(ctx context.Context, p restarter_protocol.Packet, addr *net.UDPAddr) error {
+	encoded, err := p.Encode()
 	if err != nil {
 		return err
 	}
-	var attempts int
-	for attempts < MAX_ATTEMPTS {
-		msg, err := binary.Append(nil, binary.LittleEndian, utils.MsgHeader{Ty: utils.KeepAlive})
-		if err != nil {
-			return err
-		}
-
-		n, err := r.conn.WriteToUDP(msg, udpAddr)
-		if err != nil {
-			return err
-		}
-		if n != len(msg) {
-			return fmt.Errorf("Could not send full message")
-		}
-
-		r.mu.Lock()
-		ch := r.ackMap[nodeName]
-		r.mu.Unlock()
-
-		select {
-		case <-ch:
-			log.Infof("Recevied Ack from node [%v]", nodeName)
-			return nil
-		case <-time.After(time.Second):
-			log.Infof("Timeout, trying to send KeepAlive again")
-			attempts++
-		}
+	n, err := r.conn.WriteToUDP(encoded, addr)
+	if err != nil {
+		return err
+	}
+	if n != len(encoded) {
+		return fmt.Errorf("Could not send full message")
 	}
 
-	// Handle crashed node
-	return fmt.Errorf("Never got ack")
+	r.mu.Lock()
+	ch := r.ackMap[p.Id]
+	r.mu.Unlock()
+	select {
+	case <-ch:
+		log.Infof("Received ack for message %v", p.Id)
+		r.mu.Lock()
+		delete(r.ackMap, p.Id)
+		r.mu.Unlock()
+	case <-time.After(time.Second):
+		log.Infof("Timeout, trying to send again message %v", p.Id)
+		return os.ErrDeadlineExceeded
+	case <-ctx.Done():
+		return nil
+	}
+	return nil
+}
+
+func (r *restarter) handleAck(msgId uint64) {
+	r.mu.Lock()
+	ch, ok := r.ackMap[msgId]
+	r.mu.Unlock()
+
+	if !ok {
+		log.Errorf("Channel for msg %v does not exist", msgId)
+	} else {
+		ch <- true
+	}
+}
+
+func (r *restarter) newMsgId() uint64 {
+	r.mu.Lock()
+	r.lastMsgId += 1
+	r.ackMap[r.lastMsgId] = make(chan bool)
+	defer r.mu.Unlock()
+
+	return r.lastMsgId
 }
