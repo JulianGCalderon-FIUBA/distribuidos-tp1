245c3ec514323f82456c60f57f54262986e64911
Rocío <rplatini@fi.uba.ar>
2024-11-25 22:39:01 -0300

    Refactor nodes monitoring

--- restarter-protocol/restarter.go
+++ restarter-protocol/restarter.go
@@ -10,7 +10,8 @@ import (
 	"net"
 	"os"
 	"os/exec"
-	"slices"
+	"strconv"
+	"strings"
 	"sync"
 	"time"
 
@@ -21,7 +22,7 @@ const CONFIG_PATH = ".node-config.csv"
 const MAX_ATTEMPTS = 3
 const MAX_PACKAGE_SIZE = 1024
 
-const CONTAINER_NAME = "restarter-"
+const RESTARTER_NAME = "restarter-"
 const RESTARTER_PORT = 14300
 
 var log = logging.MustGetLogger("log")
@@ -103,19 +104,6 @@ func NewRestarter(address string, id uint64, replicas uint64) *Restarter {
 	}
 }
 
-func (r *Restarter) WaitLeader(amILeader bool) {
-	r.condLeaderId.L.Lock()
-	defer r.condLeaderId.L.Unlock()
-	for amILeader != r.amILeader() {
-		r.condLeaderId.Wait()
-	}
-}
-
-// requires lock
-func (r *Restarter) amILeader() bool {
-	return (r.id == r.leaderId) && r.hasLeader
-}
-
 func (r *Restarter) Start(ctx context.Context) error {
 	var err error
 	closer := utils.SpawnCloser(ctx, r.conn)
@@ -130,7 +118,7 @@ func (r *Restarter) Start(ctx context.Context) error {
 		utils.Expect(err, "Failed to start election")
 	}()
 
-	go r.monitorNeighbor(ctx)
+	go r.monitorNode(ctx, fmt.Sprintf("%v%v", RESTARTER_NAME, (r.id+1)%r.replicas), RESTARTER_PORT)
 
 	r.read(ctx)
 
@@ -143,31 +131,32 @@ func (r *Restarter) StartMonitoring(ctx context.Context) {
 		go func(nodeName string) {
 			defer r.wg.Done()
 			time.Sleep(time.Second)
-			err := r.monitorNode(ctx, nodeName)
-			utils.Expect(err, "Failed to monitor node")
+			port, err := strconv.Atoi(r.nodes[nodeName])
+			utils.Expect(err, "Failed to parse port")
+			r.monitorNode(ctx, nodeName, port)
 		}(node)
 	}
 }
 
-func (r *Restarter) monitorNode(ctx context.Context, nodeName string) error {
-	udpAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%v:%v", nodeName, r.nodes[nodeName]))
-	utils.Expect(err, "Failed to resolve address")
-
+func (r *Restarter) monitorNode(ctx context.Context, containerName string, port int) {
+	ticker := time.NewTicker(time.Second * 5)
 	for {
 		select {
 		case <-ctx.Done():
-			log.Infof("I am no longer leader of recevied a SIGTERM")
-			return nil
-		default:
-			time.Sleep(time.Second)
-			err = r.safeSend(ctx, KeepAlive{}, udpAddr)
-			if err != nil {
-				if errors.Is(err, ErrFallenNode) {
-					err = r.restartNode(ctx, nodeName)
-					utils.Expect(err, fmt.Sprintf("Failed to restart node %v", nodeName))
-				} else {
-					return fmt.Errorf("Failed to send keep alive: %v", err)
+			return
+		case <-ticker.C:
+			msg := KeepAlive{}
+			addr, _ := utils.GetUDPAddr(containerName, port)
+			err := r.safeSend(ctx, msg, addr)
+			if errors.Is(err, ErrFallenNode) {
+				err := r.restartNode(ctx, containerName)
+				if err != nil {
+					log.Errorf("Failed to restart neighbor: %v", err)
 				}
+				continue
+			}
+			if err != nil {
+				log.Errorf("Failed to send keep alive: %v", err)
 			}
 		}
 	}
@@ -302,77 +291,6 @@ func (r *Restarter) handleAck(msgId uint64) {
 	ch <- true
 }
 
-func (r *Restarter) startElection(ctx context.Context) error {
-	log.Infof("Starting election")
-	e := &Election{Ids: []uint64{r.id}}
-	return r.sendToRing(ctx, e)
-}
-
-func (r *Restarter) handleElection(ctx context.Context, msg Election) error {
-	log.Infof("Received Election message")
-
-	if slices.Contains(msg.Ids, r.id) {
-		return r.startCoordinator(ctx, msg.Ids)
-	}
-	msg.Ids = append(msg.Ids, r.id)
-
-	return r.sendToRing(ctx, msg)
-}
-
-func (r *Restarter) startCoordinator(ctx context.Context, ids []uint64) error {
-	log.Infof("Starting coordinator")
-	leader := slices.Max(ids)
-	r.condLeaderId.L.Lock()
-	r.leaderId = leader
-	r.hasLeader = true
-	r.condLeaderId.L.Unlock()
-
-	r.condLeaderId.Signal()
-
-	log.Infof("Leader is %v", leader)
-
-	coor := Coordinator{
-		Leader: leader,
-		Ids:    []uint64{r.id},
-	}
-
-	return r.sendToRing(ctx, coor)
-}
-
-func (r *Restarter) handleCoordinator(ctx context.Context, msg Coordinator) error {
-	log.Infof("Received Coordinator message")
-
-	if slices.Contains(msg.Ids, r.id) {
-		return nil
-	}
-
-	r.condLeaderId.L.Lock()
-	r.leaderId = msg.Leader
-	r.hasLeader = true
-	r.condLeaderId.L.Unlock()
-
-	r.condLeaderId.Signal()
-
-	log.Infof("Leader is %v", msg.Leader)
-
-	msg.Ids = append(msg.Ids, r.id)
-	return r.sendToRing(ctx, msg)
-}
-
-func (r *Restarter) sendToRing(ctx context.Context, msg Message) error {
-	next := r.id + 1
-	for {
-		host := fmt.Sprintf("%v%v", CONTAINER_NAME, next%r.replicas)
-		addr, _ := utils.GetUDPAddr(host, RESTARTER_PORT)
-		err := r.safeSend(ctx, msg, addr)
-		if err == nil {
-			return nil
-		}
-		log.Infof("Neighbor %v is not answering, sending message to next one", next%r.replicas)
-		next += 1
-	}
-}
-
 func (r *Restarter) newMsgId() uint64 {
 	r.mu.Lock()
 	r.lastMsgId += 1
@@ -382,48 +300,25 @@ func (r *Restarter) newMsgId() uint64 {
 	return r.lastMsgId
 }
 
-func (r *Restarter) monitorNeighbor(ctx context.Context) {
-	ticker := time.NewTicker(time.Second * 5)
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		case <-ticker.C:
-			msg := KeepAlive{}
-			host := fmt.Sprintf("%v%v", CONTAINER_NAME, (r.id+1)%r.replicas)
-			addr, _ := utils.GetUDPAddr(host, RESTARTER_PORT)
-			err := r.safeSend(ctx, msg, addr)
-			if errors.Is(err, ErrFallenNode) {
-				err := r.restartNeighbor(ctx, (r.id+1)%r.replicas)
-				if err != nil {
-					log.Errorf("Failed to restart neighbor: %v", err)
-				}
-				continue
-			}
-			if err != nil {
-				log.Errorf("Failed to send keep alive: %v", err)
-			}
-		}
-	}
-}
-
-func (r *Restarter) restartNeighbor(ctx context.Context, id uint64) error {
-	log.Errorf("Neighbor %v has fallen. Restarting...", id)
-	if id == r.leaderId {
-		log.Infof("Leader has fallen, starting election")
-		err := r.startElection(ctx)
-		if err != nil {
-			return err
-		}
-	}
-	return r.restartNode(ctx, fmt.Sprintf("%v%v", CONTAINER_NAME, id))
-}
-
 func (r *Restarter) restartNode(ctx context.Context, containerName string) error {
-	log.Infof("Restarting [%v]", containerName)
+	if r.isNeighbor(containerName) {
+		id := (r.id + 1) % r.replicas
+		if id == r.leaderId {
+			log.Infof("Leader has fallen, starting election")
+			err := r.startElection(ctx)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	log.Errorf("Node %v has fallen. Restarting...", containerName)
 	time.Sleep(2 * time.Second) // wait if SIGTERM signal triggered
-	cmdStr := fmt.Sprintf("docker restart %v", containerName)
+	cmdStr := fmt.Sprintf("docker start %v", containerName)
 	_, err := exec.CommandContext(ctx, "/bin/sh", "-c", cmdStr).Output()
 
 	return err
+}
+
+func (r *Restarter) isNeighbor(containerName string) bool {
+	return strings.Contains(containerName, RESTARTER_NAME)
 }
