d2873694f1f4a33c64b2f03aa33f6ef35a299d0b
solmoon16 <mmoon@fi.uba.ar>
2024-11-30 21:03:43 -0300

    add disk set structure

---
+++ middleware/diskset.go
@@ -0,0 +1,79 @@
+package middleware
+
+import (
+	"bufio"
+	"distribuidos/tp1/database"
+	"encoding/binary"
+	"errors"
+	"io"
+	"io/fs"
+	"strconv"
+)
+
+type DiskSet struct {
+	name string
+	ids  map[int]struct{}
+}
+
+func NewSetDisk(name string) *DiskSet {
+	return &DiskSet{
+		name: name,
+		ids:  make(map[int]struct{}),
+	}
+}
+
+func (s *DiskSet) Mark(id int) {
+	s.ids[id] = struct{}{}
+}
+
+func (s *DiskSet) Seen(id int) bool {
+	_, ok := s.ids[id]
+	return ok
+}
+
+func (s *DiskSet) MarkDisk(snapshot *database.Snapshot, id int) error {
+	_, err := snapshot.Create(strconv.Itoa(id))
+	if err != nil {
+		return err
+	}
+
+	file, err := snapshot.Append(s.name)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(file, binary.LittleEndian, uint64(id))
+	if err != nil {
+		return err
+	}
+
+	s.Mark(id)
+	return nil
+}
+
+func (s *DiskSet) LoadDisk(db *database.Database) error {
+	file, err := db.Get(s.name)
+	var pathError *fs.PathError
+	if errors.As(err, &pathError) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	reader := bufio.NewReader(file)
+
+	for {
+		var id uint64
+		err = binary.Read(reader, binary.LittleEndian, &id)
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		s.Mark(int(id))
+	}
+
+	return nil
+}
